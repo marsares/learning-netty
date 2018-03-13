2fa0014788191d9b0472fc2619576542b04d4fb5
yb <914681980@qq.com>
2018-03-13 21:27:54 +0800

    learning netty project

---
+++ src/main/java/com/marsares/org/learning_netty/EchoServer.java
@@ -0,0 +1,48 @@
+package com.marsares.org.learning_netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by marsares on 18/3/10.
+ */
+public class EchoServer {
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws Exception{
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+        try{
+            ServerBootstrap b=new ServerBootstrap();
+            b.group(group).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port)).childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel socketChannel) throws Exception {
+                    socketChannel.pipeline().addLast(serverHandler);
+                }
+            });
+            ChannelFuture f=b.bind().sync();
+            f.channel().closeFuture().sync();
+        }finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[]args) throws Exception{
+        if(args.length!=1){
+            System.out.println("error, need one input");
+        }
+        int port=Integer.parseInt(args[0]);
+        new EchoServer(port).start();
+    }
+}
